ff20ce408df4f86792abb818a04fff48965b3409
miku-nosuke <iwk@mac.com>
2025-01-04 09:39:58 +0900

    Add test cases for StudyLogsController

--- test/controllers/study_logs_controller_test.rb
+++ test/controllers/study_logs_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class StudyLogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @study_log = study_logs(:one)
+  end
+
+  test "should get index" do
+    get study_logs_url, as: :json
+    assert_response :success
+  end
+
+  test "should create study_log" do
+    assert_difference("StudyLog.count") do
+      post study_logs_url, params: { study_log: { title: "New Log", content: "Testing", duration: 2 } }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should show study_log" do
+    get study_log_url(@study_log), as: :json
+    assert_response :success
+  end
+
+  test "should update study_log" do
+    patch study_log_url(@study_log), params: { study_log: { title: "Updated Log" } }, as: :json
+    assert_response :success
+  end
+
+  test "should destroy study_log" do
+    assert_difference("StudyLog.count", -1) do
+      delete study_log_url(@study_log), as: :json
+    end
+    assert_response :no_content
+  end
 end
